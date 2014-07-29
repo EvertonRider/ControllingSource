6ed604492d0d64c3e4206533d1139f57c11057f4
Everton <everton.rider@gmail.com>
2014-07-29 15:57:01 -0300

    New File

---
+++ ControllingSource/EMVViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMVViewController.h
+//  ControllingSource
+//
+//  Created by Everton on 29/07/14.
+//  Copyright (c) 2014 Everton Miranda Vitorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMVViewController : UIViewController
+
+@end
---
+++ ControllingSource/EMVViewController.m
@@ -0,0 +1,38 @@
+//
+//  EMVViewController.m
+//  ControllingSource
+//
+//  Created by Everton on 29/07/14.
+//  Copyright (c) 2014 Everton Miranda Vitorio. All rights reserved.
+//
+
+#import "EMVViewController.h"
+
+@interface EMVViewController ()
+
+@end
+
+@implementation EMVViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
